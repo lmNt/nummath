d6aa68f7c9948eb2b3927e04c9efa717f3159bb4
Merikan <merikankoyun@gmail.com>
2014-06-03 16:04:34 +0200

    fixed bug in heron

--- ueb5/heron.c
+++ ueb5/heron.c
@@ -7,22 +7,24 @@ double heron(double w, double x0, double eps)
    int iter = 0;
    double x = x0;
 
-   while ((x*x - w) > eps)
+   while (fabs(x*x - w) > eps)
    {
       x = 0.5 * (x + w/x);
       iter++;
    }
+   x = x > 0 ? x : -x;
+
    printf("Number of iterations: %d \n", iter);
    return x;
 }
 
 
-int main(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
    double res, w, x0, eps;
    w   = 2;
    eps = 10e-8;
-   x0  = 10000;
+   x0  = -100;
 
    res = heron(w, x0, eps);
    printf("Result: %.15f\n", res);
